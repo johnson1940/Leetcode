124bb8bdd93a95f35a3a6884e426d8d106671d63
Johnson <116719711+johnson1940@users.noreply.github.com>
2025-04-24 14:00:21 +0530

    Time: 0 ms (100%), Space: 19.1 MB (11.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            com = target - num
-            if com in num_map:
-                return [num_map[com], i]
-            num_map[num] = i
-        return num_map[num]        
-               
+        two_sum_pair  = {}
 
-        
+        for i, num in enumerate(nums):
+            if target - num in two_sum_pair:
+                return [i, two_sum_pair[target - num]]
+            two_sum_pair[num] = i    
