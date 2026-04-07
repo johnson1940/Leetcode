afa729f15c82142d2ecfbcb91cb13aa7a662e335
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-07 23:05:04 +0530

    Time: 6 ms (73.61%), Space: 153.8 MB (5.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,13 +1,27 @@
 class Solution {
-  List<int> twoSum(List<int> nums, int target) {
-    List<int> results = [];
-    for(int i = 0; i < nums.length ; i++) {
-        for(int j = i + 1; j < nums.length; j ++) {
-            if(nums[i] + nums[j] == target) {
-                results = [i, j];
+//   List<int> twoSum(List<int> nums, int target) {
+//     List<int> results = [];
+//     for(int i = 0; i < nums.length ; i++) {
+//         for(int j = i + 1; j < nums.length; j ++) {
+//             if(nums[i] + nums[j] == target) {
+//                 results = [i, j];
+//             }
+//         }
+//     }
+//     return results;
+//   }
+
+    List<int> twoSum(List<int> nums, int target) {
+        Map<int, int> map = {};
+        for(int i = 0 ; i < nums.length ; i ++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) {
+                return [map[complement]!, i];
+            }
+            else {
+                map[nums[i]] = i;
             }
         }
+        return [];
     }
-    return results;
-  }
 }
