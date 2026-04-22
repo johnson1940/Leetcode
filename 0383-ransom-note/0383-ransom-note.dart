780c4c070979d3aae74860b21c0c807b1e309973
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-22 12:05:39 +0530

    Time: 23 ms (56.52%), Space: 148.8 MB (65.22%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.dart
+++ 0383-ransom-note/0383-ransom-note.dart
@@ -1,17 +1,16 @@
 class Solution {
   bool canConstruct(String ransomNote, String magazine) {
       
-    Map<String, int> map = {};
+     Map<String, int> resultMap = {};
 
-    for(String char in magazine.split("")) {
-        map[char] = (map[char] ?? 0) + 1;
-    }
+     for(String char in magazine.split("")) {
+        resultMap[char] = (resultMap[char] ?? 0) + 1;
+     }
 
-    for(String char in ransomNote.split("")) {
-        map[char] = (map[char] ?? 0) - 1;
-        if((map[char] ?? 0) < 0) return false;
-    }
+     for(String char in ransomNote.split("")) {
+        resultMap[char] = (resultMap[char] ?? 0) - 1;
+        if(resultMap[char]! < 0) return false;
+     }
 
-    return true;
-  }
-}
+     return true;
+}}
