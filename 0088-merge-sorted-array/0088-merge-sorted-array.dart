bdc826d9f1f811c296a0e7f6a284f55e06a52306
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-23 12:30:05 +0530

    Time: 5 ms (34.56%), Space: 152.5 MB (5.15%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.dart
@@ -0,0 +1,21 @@
+class Solution {
+  void merge(List<int> nums1, int m, List<int> nums2, int n) {
+        List<int> result = [];
+
+        
+        nums1.removeWhere((num) {
+           if (num == 0 && n > 0) {
+               n--;
+               return true;
+             }
+            return false;
+        });
+
+        for(int i = 0 ; i < nums2.length ; i++) {
+            nums1.add(nums2[i]);
+        }
+        nums1.sort();
+        // nums1 = [...nums1, ...nums2];
+        // nums1.sort();
+  }
+}
