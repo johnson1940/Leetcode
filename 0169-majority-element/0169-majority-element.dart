b0a51a17fca1da333b08fe3c4fa8035d91db1b36
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-23 15:50:05 +0530

    Time: 7 ms (51.02%), Space: 153.7 MB (60.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.dart
+++ 0169-majority-element/0169-majority-element.dart
@@ -1,20 +1,20 @@
 class Solution {
   int majorityElement(List<int> nums) {
-     
-      Map<int, int> counter = {};
+        
+      Map<int, int> counterMap = {};
+
       int result = 0;
 
-      for(int nu in nums) {
-        counter[nu] = (counter[nu] ?? 0) + 1;
+      for(int n in nums) {
+        counterMap[n] = (counterMap[n] ?? 0) + 1;
       }
 
-      counter.forEach((num, count) {
-        if(count > nums.length/2) {
-          result = num;
-        }
+      counterMap.forEach((key, value) {
+         if(value > nums.length / 2) {
+            result = key;
+         }
       });
-
-      return result;
+    return result;
 
   }
 }
