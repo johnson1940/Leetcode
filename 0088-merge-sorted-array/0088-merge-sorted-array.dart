d0605fdc61885c1aabc5bbb7123df565abf4b039
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-23 13:16:00 +0530

    Time: 19 ms (5.15%), Space: 147.9 MB (78.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.dart
+++ 0088-merge-sorted-array/0088-merge-sorted-array.dart
@@ -1,19 +1,41 @@
 class Solution {
   void merge(List<int> nums1, int m, List<int> nums2, int n) {
-        List<int> result = [];
-        
-        nums1.removeWhere((num) {
-           if (num == 0 && n > 0) {
-               n--;
-               return true;
-             }
-            return false;
-        });
 
-        for(int i = 0 ; i < nums2.length ; i++) {
-            nums1.add(nums2[i]);
-        }
-        nums1.sort();
+       // step 1
+       int p1 = m - 1;
+       int p2 = n - 1;
+       int p = m+n-1;
+       
+       while(p2 >= 0) {
+          // step 2
+          if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+             nums1[p] = nums1[p1];
+             p1--;
+          }
+          // step 3
+          else {
+            nums1[p] = nums2[p2];
+            p2--;
+          }
+          p--;
+       }
+        
+        
+        // works but this is not optimal 
+        // List<int> result = [];
+        
+        // nums1.removeWhere((num) {
+        //    if (num == 0 && n > 0) {
+        //        n--;
+        //        return true;
+        //      }
+        //     return false;
+        // });
+
+        // for(int i = 0 ; i < nums2.length ; i++) {
+        //     nums1.add(nums2[i]);
+        // }
+        // nums1.sort();
 
   }
 }
