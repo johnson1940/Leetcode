afcdac69cb78a3a885b4b15f684eb070c4605bf7
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-23 17:49:42 +0530

    Time: 1 ms (100%), Space: 156.2 MB (100%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.dart
+++ 0189-rotate-array/0189-rotate-array.dart
@@ -1,6 +1,11 @@
 class Solution {
   void rotate(List<int> nums, int k) {
+    
+    // nums = [1,2,3]  n=3
 
+    // rotate 3 times → [1,2,3]  ← same as start!
+    // rotate 4 times → [3,1,2]  ← same as rotating 1 time
+    // rotate 6 times → [1,2,3]  ← same as rotating 0 times
     k = k % nums.length;
     int start = 0;
     int end = nums.length - 1;
