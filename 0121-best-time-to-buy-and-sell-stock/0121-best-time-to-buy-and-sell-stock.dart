980a6a590a5faa0e12cfa6d20bc9781a632d75b5
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-04-30 22:12:27 +0530

    Time: 5 ms (30.63%), Space: 188.1 MB (99.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
@@ -1,19 +1,19 @@
 class Solution {
   int maxProfit(List<int> prices) {
-    int minPrice = prices[0];
-    int maxPrice = 0;
-
-    for(int i = 1 ; i < prices.length ; i++) {
-        if(prices[i] < minPrice) {
-            minPrice = prices[i];
+    int l = 0;
+    int r = 1;
+    int profit = 0;
+    int maxProfit = 0;
+    while(r < prices.length) {
+        if(prices[l] < prices[r]) {
+            profit = prices[r] - prices[l];
+            maxProfit = max(maxProfit, profit);
         }
-        else {
-            int profit = prices[i] - minPrice;
-            if(profit > maxPrice) {
-                maxPrice = profit;
-            }
+        else { 
+          l = r;
         }
+        r++;
     }
-    return maxPrice;
+    return maxProfit;
   }
 }
