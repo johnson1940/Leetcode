0d7cb6bc2ca23b204317b936231c0e0f62ea9b69
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-02 13:57:05 +0530

    Time: 1 ms (100%), Space: 149.4 MB (84.72%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.dart
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.dart
@@ -1,22 +1,20 @@
 class Solution {
   List<int> twoSum(List<int> numbers, int target) {
-    int left_pointer = 0;
-    int right_pointer = numbers.length - 1;
-    List<int> result = [];
-     
-     while(left_pointer < right_pointer) {
-        int sum = numbers[left_pointer] + numbers[right_pointer];
+      int low = 0;
+      int high = numbers.length - 1;
+
+      while(low < high) {
+        int sum = numbers[low] + numbers[high];
         if(sum == target) {
-            return [left_pointer+1, right_pointer+1];
+            return [low+1, high+1];
         }
         else if(sum > target) {
-            right_pointer--;
+            high--;
         }
         else {
-            left_pointer++;
+            low++;
         }
-     }
-     return [];
-
+      }
+    return [];
   }
 }
