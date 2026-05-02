495de74332e25d76ebef7427ecadcb9e9eab1a79
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-02 15:38:50 +0530

    Time: 0 ms (100%), Space: 149.6 MB (98.31%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.dart
+++ 0344-reverse-string/0344-reverse-string.dart
@@ -1,16 +1,13 @@
 class Solution {
-    reverseString(List<String> s) {
-
-    int first_point = 0;
-    int second_point = s.length - 1;
-
-    while(first_point < second_point) {
-        String temp = s[first_point];
-        s[first_point] = s[second_point];
-        s[second_point] = temp;
-        first_point++;
-        second_point--;
-    }
-    return s;
+  void reverseString(List<String> s) {
+      int low = 0;
+      int high = s.length - 1;
+        while(low < high) {
+          String temp = s[low];
+          s[low] = s[high];
+          s[high] = temp;
+          low++;
+          high--;
+     }
   }
 }
