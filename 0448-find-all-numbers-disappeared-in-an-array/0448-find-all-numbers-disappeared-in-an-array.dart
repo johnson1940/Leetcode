1a258d4eb994d9c2a4761db1ae29ef74d40aadd5
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-05 15:46:32 +0530

    Time: 34 ms (22.5%), Space: 177.3 MB (37.5%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.dart
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.dart
@@ -1,15 +1,14 @@
 class Solution {
   List<int> findDisappearedNumbers(List<int> nums) {
-    
-    Set<int> seen = nums.toSet();
-
-    List<int> result = [];
-
-    for(int i = 1 ; i <= nums.length ; i++) {
-        if(!seen.contains(i)) {
-            result.add(i);
-        }
+  Set<int> seen = nums.toSet();
+  
+  List<int> result = [];
+  for (int i = 1; i <= nums.length; i++) {
+    if (!seen.contains(i)) {
+      result.add(i);
     }
-    return result;
+  }
+  
+  return result;
   }
 }
