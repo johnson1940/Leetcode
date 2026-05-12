356d7995370022e051f8ea76df98167b9c379da4
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-12 17:45:47 +0530

    Time: 0 ms (100%), Space: 145.9 MB (100%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.dart
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.dart
@@ -7,18 +7,30 @@
  * }
  */
 class Solution {
-  ListNode? middleNode(ListNode? head) {
-    int length = 0;
-    ListNode? current = head;
-    while(current != null) {
-        length++;
-        current = current.next;
+//   ListNode? middleNode(ListNode? head) {
+//     int length = 0;
+//     ListNode? current = head;
+//     while(current != null) {
+//         length++;
+//         current = current.next;
+//     }
+//     int middleIndex = length ~/ 2;
+//     current = head;
+//     for(int i = 0 ; i < middleIndex; i++) {
+//         current = current!.next;
+//     }
+//    return current; 
+//   }
+
+    ListNode? middleNode(ListNode? head) {
+
+        ListNode? slow = head;
+        ListNode? fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow!.next;
+            fast = fast.next!.next;
+        }
+       return slow;    
     }
-    int middleIndex = length ~/ 2;
-    current = head;
-    for(int i = 0 ; i < middleIndex; i++) {
-        current = current!.next;
-    }
-   return current; 
-  }
 }
