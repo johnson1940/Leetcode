5dd2711abc74299a99da2508ea01fd92e0577007
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-15 15:19:48 +0530

    Time: 3 ms (100%), Space: 179.1 MB (95.83%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.dart
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.dart
@@ -7,23 +7,55 @@
  * }
  */
 class Solution {
-  bool isPalindrome(ListNode? head) {
-    List<int> arr = [];
-    ListNode? current = head;
+  // Method 1  
+//   bool isPalindrome(ListNode? head) {
+//     List<int> arr = [];
+//     ListNode? current = head;
 
-    while(current!= null) {
-        arr.add(current.val);
-        current = current.next;
-    }
+//     while(current!= null) {
+//         arr.add(current.val);
+//         current = current.next;
+//     }
      
-    int left = 0;
-    int right = arr.length - 1;
+//     int left = 0;
+//     int right = arr.length - 1;
 
-    while(left < right) {
-        if(arr[left] != arr[right]) return false;
-        left++;
-        right--;
-    }
-   return true;
-  }
+//     while(left < right) {
+//         if(arr[left] != arr[right]) return false;
+//         left++;
+//         right--;
+//     }
+//    return true;
+//   }
+
+   bool isPalindrome(ListNode? head) {
+      if(head == null || head.next == null) return true;
+      // Step 1 finding the middle of the linked list
+      ListNode? slow = head;
+      ListNode? fast = head;
+      while(fast != null && fast.next != null) {
+        slow = slow!.next;
+        fast = fast.next!.next;
+      }
+
+      // Step 2 reversing the second half
+      ListNode? prev = null;
+      ListNode? current = slow;
+      while(current != null) {
+        ListNode? next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+      }
+
+      /// Step 3 compare the second half and the first half
+      ListNode? left = head;
+      ListNode? right = prev;
+      while(right != null && left != null) {
+        if(left.val != right.val) return false;
+        left = left.next;
+        right = right.next;
+      }
+    return true;  
+   }
 }
