09a63a53d00b16d57173f8a0c84093fef6393381
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-18 12:46:52 +0530

    Time: 3 ms (100%), Space: 252.8 MB (75%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.dart
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? deleteMiddle(ListNode? head) {
+
+
+     if(head!.next == null) {
+       head = null;  
+       return head;
+      } 
+
+      ListNode? fast = head;
+      ListNode? slow = head;
+      ListNode? prev;
+
+      while(fast != null && fast.next != null) {
+        prev = slow;
+        slow = slow!.next;
+        fast = fast.next!.next;
+      }
+
+    prev!.next = slow!.next;   
+    return head;
+  }
+}
