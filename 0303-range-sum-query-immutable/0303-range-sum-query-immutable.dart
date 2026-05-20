524fa95dde6c7cfff10d637677f781e4517b87a3
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-20 13:33:31 +0530

    Time: 2 ms (88.89%), Space: 161.4 MB (88.89%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.dart
@@ -0,0 +1,21 @@
+class NumArray {
+  List<int> prefix = [];
+  NumArray(List<int> nums) {
+    prefix = List.filled(nums.length, 0);
+    prefix[0] = nums[0];
+    for(int i = 1; i < nums.length ; i++) {
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+  }
+  
+  int sumRange(int left, int right) {
+    if(left == 0) return prefix[right];
+    return prefix[right] - prefix[left - 1];
+  }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = NumArray(nums);
+ * int param1 = obj.sumRange(left,right);
+ */
