e832f00a5d9c9b0307c59260fbc1ba80ea3a27fb
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-28 12:02:54 +0530

    Time: 1 ms (67.07%), Space: 151.9 MB (35.37%) - LeetHub

--- 0136-single-number/0136-single-number.dart
+++ 0136-single-number/0136-single-number.dart
@@ -1,18 +1,24 @@
 class Solution {
-  int singleNumber(List<int> nums) {
-  Map<int, int> counterMap = {};
-  int result = 0;
-  
-  for(int i = 0; i < nums.length ; i++) {
-    counterMap[nums[i]] = (counterMap[nums[i]] ?? 0) + 1;
-  }
-  
-  counterMap.forEach((key, value) {
-     if(value == 1) {
-       result = key;
-     }
-  });
-  
+int singleNumber(List<int> nums) {
+    int result = 0;
+    for(int num in nums) {
+        result ^= num;
+    }
     return result;
+
+//   Map<int, int> counterMap = {};
+//   int result = 0;
+  
+//   for(int i = 0; i < nums.length ; i++) {
+//     counterMap[nums[i]] = (counterMap[nums[i]] ?? 0) + 1;
+//   }
+  
+//    counterMap.forEach((key, value) {
+//      if(value == 1) {
+//        result = key;
+//      }
+//    });
+  
+//     return result;
   }
 }
