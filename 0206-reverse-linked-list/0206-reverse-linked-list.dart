2fd148ae46caa576e735c07a43e7a45e519684a8
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-05-29 15:52:53 +0530

    Time: 0 ms (100%), Space: 149.6 MB (50%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.dart
+++ 0206-reverse-linked-list/0206-reverse-linked-list.dart
@@ -8,21 +8,17 @@
  */
 class Solution {
   ListNode? reverseList(ListNode? head) {
-     ListNode? previous = null;
-     ListNode? current = head;
-     ListNode? next;
+    ListNode? prev = null;
+    ListNode? current = head;
+    ListNode? next;
 
-     while(current != null) {
-        // Step 1 save the next first
-        next = current.next;
-        // step 2 
-        current.next = previous;
-        // step 3
-        previous = current;
-        // step 4
-        current = next;
-
-     }
-   return previous;
+    while(current != null) {
+       next = current!.next;
+       current.next = prev;
+       prev = current;
+       current = next;
+    }
+   return prev;
   }
+     
 }
