8194b89080b6d3bd9cb3d3b6c04f699b6ca13a5f
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-04 12:38:07 +0530

    Time: 0 ms (100%), Space: 147.2 MB (80.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.dart
+++ 0198-house-robber/0198-house-robber.dart
@@ -1,14 +1,29 @@
 class Solution {
+//    int rob(List<int> nums) {
+//       if(nums.isEmpty) return 0;
+//       if(nums.length == 1) return nums[0];
+//       List<int> houses = List.filled(nums.length, 0);
+//       houses[0] = nums[0];
+//       houses[1] = max(nums[0], nums[1]);
+
+//       for(int i = 2 ; i < nums.length ; i++) {
+//         houses[i] = max(houses[i - 1], nums[i] + houses[i - 2]);
+//       }
+//     return houses[nums.length - 1];  
+//   }
+   
+   /// For memory optimization
    int rob(List<int> nums) {
       if(nums.isEmpty) return 0;
       if(nums.length == 1) return nums[0];
-      List<int> houses = List.filled(nums.length, 0);
-      houses[0] = nums[0];
-      houses[1] = max(nums[0], nums[1]);
+      int prev2 = nums[0];
+      int prev1 = max(nums[0], nums[1]);
 
       for(int i = 2 ; i < nums.length ; i++) {
-        houses[i] = max(houses[i - 1], nums[i] + houses[i - 2]);
+        int current = max(prev1, nums[i] + prev2);
+        prev2 = prev1;
+        prev1 = current;
       }
-    return houses[nums.length - 1];  
+    return prev1;  
   }
 }
