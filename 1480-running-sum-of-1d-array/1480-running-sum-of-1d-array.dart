8709e1ccda65268e7d46920f51f1e198e275e532
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-04 16:07:36 +0530

    Time: 0 ms (100%), Space: 147 MB (96.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.dart
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.dart
@@ -3,9 +3,7 @@ class Solution {
      List<int> result = List.filled(nums.length, 0);
      result[0] = nums[0];
 
-      for(int i = 1 ; i < nums.length; i++) {
-        result[i] = result[i - 1] + nums[i];
-      }
+      for(int i = 1 ; i < nums.length; i++) result[i] = result[i - 1] + nums[i];
     return result;  
   }
 }
