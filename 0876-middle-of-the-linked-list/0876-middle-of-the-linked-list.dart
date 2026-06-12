12db95f4e9ebf5452a7417feddc530b98c462556
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-12 12:13:19 +0530

    Time: 0 ms (100%), Space: 149.7 MB (31.82%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.dart
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.dart
@@ -23,14 +23,13 @@ class Solution {
 //   }
 
     ListNode? middleNode(ListNode? head) {
+       ListNode? slow = head;
+       ListNode? fast = head;
 
-        ListNode? slow = head;
-        ListNode? fast = head;
-
-        while(fast != null && fast.next != null) {
-            slow = slow!.next;
-            fast = fast.next!.next;
-        }
-       return slow;    
+       while(fast != null && fast.next != null) {
+         slow = slow!.next;
+         fast = fast.next!.next;
+       }
+    return slow;    
     }
 }
