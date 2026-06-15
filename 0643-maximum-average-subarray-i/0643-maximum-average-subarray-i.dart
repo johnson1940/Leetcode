2231fac8939961d073bd06285a98bcabb1d65689
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-15 11:52:44 +0530

    Time: 2 ms (72.22%), Space: 176.5 MB (13.89%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.dart
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.dart
@@ -1,18 +1,18 @@
 class Solution {
   double findMaxAverage(List<int> nums, int k) {
-     int windowSum = 0;
+        int windowSum = 0;
 
-     for(int i = 0; i < k ; i++) {
-        windowSum += nums[i];
-     }
+        for(int i = 0; i < k ; i++) {
+            windowSum += nums[i];
+        }
 
-     int maxSumAverage = windowSum;
+        int maxWindowSum = windowSum;
 
-     for(int i = k ; i < nums.length ; i++) {
-        windowSum += nums[i];
-        windowSum -= nums[i-k];
-        maxSumAverage = max(windowSum, maxSumAverage);
-     }
-     return maxSumAverage / k;
+        for(int i = k ; i < nums.length ; i++) {
+            windowSum += nums[i];
+            windowSum -= nums[i - k];
+            maxWindowSum = max(windowSum, maxWindowSum);
+        }
+    return maxWindowSum / k;    
   }
 }
