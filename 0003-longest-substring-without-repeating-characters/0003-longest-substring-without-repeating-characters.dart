63ab6d41c97e2a22dae7114f31ad69231e1f04fc
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-17 13:19:15 +0530

    Time: 19 ms (38.55%), Space: 149.3 MB (62.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.dart
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.dart
@@ -1,17 +1,17 @@
 class Solution {
   int lengthOfLongestSubstring(String s) {
-    Set<String> window = {};
-    int L = 0;
-    int maxLength = 0;
+       Set<String> charSet = {};
+       int left = 0;
+       int maxWindowLength = 0;
 
-    for(int R = 0 ; R < s.length ; R++) {
-        while(window.contains(s[R])) {
-            window.remove(s[L]);
-            L++;
-        }
-        window.add(s[R]);
-        maxLength = max(maxLength, R - L + 1);
-    }
-    return maxLength;
+       for(int right = 0 ; right < s.length ; right++) {
+           while(charSet.contains(s[right])) {
+               charSet.remove(s[left]);
+               left++;
+           }
+        charSet.add(s[right]);
+        maxWindowLength = max(maxWindowLength, right - left + 1);   
+       }
+    return maxWindowLength;   
   }
 }
