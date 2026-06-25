1069f24e8c4606cae761983366a0093060fc6340
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-25 12:26:35 +0530

    Time: 67 ms (13.98%), Space: 158.5 MB (54.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.dart
+++ 0049-group-anagrams/0049-group-anagrams.dart
@@ -1,12 +1,30 @@
 class Solution {
-  List<List<String>> groupAnagrams(List<String> strs) {
-    Map<String , List<String>> map = {};
+  /// o(m * (n logn))
+//   List<List<String>> groupAnagrams(List<String> strs) {
+//     Map<String , List<String>> map = {};
 
-    for(String word in strs) {
-       String key = (word.split('')..sort()).join('');
-       map[key] ??= [];
-       map[key]!.add(word);
-    }
-    return map.values.toList();
-  }
+//     for(String word in strs) {
+//        String key = (word.split('')..sort()).join('');
+//        map[key] ??= [];
+//        map[key]!.add(word);
+//     }
+//     return map.values.toList();
+//   }
+
+
+   List<List<String>> groupAnagrams(List<String> strs) {
+        Map<String, List<String>> map = {};
+
+        for(String s in strs) {
+
+            List<int> count = List.filled(26, 0);
+
+            for(int i = 0 ; i < s.length ; i++) {
+                count[s.codeUnitAt(i) - 97] += 1;
+            }
+          String key = count.join(',');
+          map.putIfAbsent(key, () => []).add(s);  
+        }
+    return map.values.toList();    
+   }
 }
