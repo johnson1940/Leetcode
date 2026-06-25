798904558b0905bb551e6e92dac9b249f2135191
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-25 13:25:39 +0530

    Time: 17 ms (64.81%), Space: 154.3 MB (90.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.dart
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.dart
@@ -1,19 +1,33 @@
 class Solution {
   List<int> topKFrequent(List<int> nums, int k) {
-      Map<int, int> counterMap = {};
       
-      for(int number in nums) {
-        counterMap[number] = (counterMap[number] ?? 0) + 1;
-      }
+      Map<int, int> count = {};
 
-      var sortedEntries = counterMap.entries.toList()
-        ..sort((a, b) => b.value.compareTo(a.value));
+      // Step 1 -> Fill the bucket with size of the input
+      List<List<int>> freq = List.generate(nums.length + 1, 
+                            (_) => <int>[]);
 
-     List<int> result = sortedEntries
-      .map((entry) => entry.key)
-      .take(k)
-      .toList();
+      // Step 2 -> count the freq of the element
+      for(int n in nums) {
+         count[n] = (count[n] ?? 0) + 1;
+      } 
 
-    return result;  
+      // Step 3 -> Fill the bucket
+      count.forEach((key, value) {
+        freq[value].add(key);
+      }); 
+
+      // Step 4 -> Taking the first k elements
+      List<int> result = [];
+
+      for(int i = freq.length - 1; i > 0; i--) {
+           for(int num in freq[i]) {
+              result.add(num);
+              if(result.length == k) {
+                return result;
+              }
+           }
+      }    
+    return [];           
   }
 }
