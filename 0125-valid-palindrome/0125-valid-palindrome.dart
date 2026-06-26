4747c3714a5aa6ffb360dd579be59516f139a985
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-26 11:44:06 +0530

    Time: 10 ms (78.03%), Space: 151.2 MB (36.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.dart
+++ 0125-valid-palindrome/0125-valid-palindrome.dart
@@ -1,15 +1,20 @@
 class Solution {
   bool isPalindrome(String s) {
-  String alpha = s.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '').toLowerCase();
-  int low = 0;
-  int high = alpha.length - 1;
-  while(low < high) {
-    if(alpha[low] != alpha[high]) {
-      return false;
+    String nonAlpha = s.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
+    String cleaned = nonAlpha.toLowerCase();
+    
+    int left_pointer = 0;
+    int right_pointer = cleaned.length - 1;
+
+    while(left_pointer < right_pointer) {
+        if(cleaned[left_pointer] == cleaned[right_pointer]) {
+            left_pointer++;
+            right_pointer--;
+        }
+        else {
+            return false;
+        }
     }
-      low++;
-      high--;
+    return true;
   }
-  return true;
-}
 }
