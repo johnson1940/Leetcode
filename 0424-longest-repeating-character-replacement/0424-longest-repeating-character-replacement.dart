56efd319337064be32f50194690e6727e479c9e0
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-27 15:14:00 +0530

    Time: 47 ms (8%), Space: 150 MB (36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.dart
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.dart
@@ -1,28 +1,21 @@
 class Solution {
   int characterReplacement(String s, int k) {
-      Map<String, int> freqMap = {};
-      int maxFreq = 0;
-      int maxLength = 0;
-      int L = 0;
+     Map<String , int> count = {};
+     int left = 0;
+     int result =  0;
 
-      for(int R = 0; R < s.length ; R++) {
-        // ABAB K= 1
-        freqMap[s[R]] = (freqMap[s[R]] ?? 0) + 1;
-        // {A : 2 , B : 2}
-        // We are finding the max freq element 
-        maxFreq = max(maxFreq, freqMap[s[R]]!);
+     for(int right = 0 ; right < s.length ; right++) {
+         
+         count[s[right]] = (count[s[right]] ??  0) + 1;
 
-      /// length of the string - maxFreq > k -> number of replacement
-      while((R - L + 1) - maxFreq > k) {
-         /// we are gonna slide the window
-         freqMap[s[L]] = (freqMap[s[L]] ?? 0) - 1;
-         L++;
-      }
-      
-      /// we are gonna find the max Length of repeating characters
-      /// comparing with the window length
-      maxLength = max(maxLength, R - L + 1);
-      }
-      return maxLength;
+         if((right - left + 1) - count.values.reduce(max) > k) {
+             
+             count[s[left]] = (count[s[left]] ?? 0) - 1;
+
+             left++;
+         }
+        result = max(result ,right - left + 1); 
+     }
+    return result; 
   }
 }
