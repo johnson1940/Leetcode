3298f24956fbc627d2f7241bf9f8a58a0c613f2c
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-06-29 13:14:24 +0530

    Time: 6 ms (41.18%), Space: 146.3 MB (88.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.dart
+++ 0567-permutation-in-string/0567-permutation-in-string.dart
@@ -2,49 +2,32 @@ class Solution {
   bool checkInclusion(String s1, String s2) {
 
      if(s1.length > s2.length) return false;  
-     // step 1, create two maps to find the frequency
+       
+     List<int> s1Freq = List.filled(26, 0);
+     List<int> s2Freq = List.filled(26, 0);
 
-     Map<String, int> freqMap1 = {};
-     Map<String, int> freqMap2 = {};
+     for(int i = 0 ; i < s1.length ; i++) {
+        s1Freq[s1.codeUnitAt(i) - 97]++;
+        s2Freq[s2.codeUnitAt(i) - 97]++;
+     }
 
-     /// step 2, have a frequency map of s1 and s2
-    
-    for(int i = 0; i < s1.length; i++) {
-        freqMap1[s1[i]] = (freqMap1[s1[i]] ?? 0) + 1;
-    }
+     if(arraysEqual(s1Freq, s2Freq)) return true;
 
-    for(int i = 0; i < s1.length ; i++) {
-        freqMap2[s2[i]] = (freqMap2[s2[i]] ?? 0) + 1;
-    }
-   
-    /// step 2 Is to compare the two maps;
-    if(mapsEqual(freqMap1, freqMap2)) return true;
-
-    /// step 3 if it is not equals then I have to slide the window.
-
-    for(int R = s1.length; R < s2.length ; R++) {
-        freqMap2[s2[R]] = (freqMap2[s2[R]] ?? 0) + 1;
-
-        String left = s2[R - s1.length];
-        
-        /// removing the sliden character.
-        freqMap2[left] = (freqMap2[left] ?? 0) - 1;
-        if(freqMap2[left] == 0) freqMap2.remove(left);
-
-        if(mapsEqual(freqMap1, freqMap2)) return true;
-
-    }
+     for(int right = s1.length ; right < s2.length ; right++) {
+        s2Freq[s2.codeUnitAt(right) - 97]++;
+        s2Freq[s2.codeUnitAt(right - s1.length) - 97]--;
+        if(arraysEqual(s1Freq, s2Freq)) return true;
+     }
 
     return false;
   }
 
-
-  /// since mapsEqual method is not allowed we are processing with our own method
-  bool mapsEqual(Map<String, int> a, Map<String, int> b) {
-  if (a.length != b.length) return false;
-  for (String key in a.keys) {
-    if (a[key] != b[key]) return false;
+  bool arraysEqual(List<int> a, List<int> b) {
+     for(int i = 0; i < 26; i++) {
+        if(a[i] != b[i]) return false;
+      }
+    return true;
   }
-  return true;
- }
+
 }
+
