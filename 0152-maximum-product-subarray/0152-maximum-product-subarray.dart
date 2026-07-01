3e0737369152874fcd67eddfc54e36fcc8e7568f
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-07-01 12:48:06 +0530

    Time: 9 ms (16.67%), Space: 151.8 MB (16.67%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.dart
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.dart
@@ -1,14 +1,22 @@
 class Solution {
   int maxProduct(List<int> nums) {
-     int max_prod = nums[0];
-     int current_prod = nums[0];
-     int min_prod = nums[0];
-     for(int i = 1; i < nums.length ; i++) {
-        int tempMax = current_prod;
-        current_prod = max(nums[i], max(nums[i] * current_prod, nums[i] * min_prod));
-        min_prod = min(nums[i], min(nums[i] * tempMax, nums[i] * min_prod));
-        max_prod = max(current_prod, max_prod);
+     
+     int current_min = 1;
+     int current_max = 1;
+     int result = nums.reduce(max);
+
+     for(int n in nums) {
+        if(n == 0) {
+            current_min = 1;
+            current_max = 1;
+            continue;
+        }
+       int temp = n * current_max;
+       current_max = max(n, max(n * current_max , n * current_min));
+       current_min = min(n, min(temp , n * current_min));
+
+       result = max(result, current_max);
      }
-    return max_prod; 
+    return result; 
   }
 }
