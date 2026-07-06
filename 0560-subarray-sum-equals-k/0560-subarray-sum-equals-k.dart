7d878ecc20e192206043cfa6515bc21eb44c3ac1
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-07-06 21:22:19 +0530

    Time: 29 ms (57.58%), Space: 162.2 MB (84.85%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.dart
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.dart
@@ -1,18 +1,17 @@
 class Solution {
   int subarraySum(List<int> nums, int k) {
-     Map<int , int> counterMap = {0:1};
-     int count = 0;
-     int prefixSum = 0;
+      Map<int, int> counterMap = {0 : 1};
+      int count = 0;
+      int prefixSum = 0;
 
-     for(int num in nums) {
+      for(int num in nums) {
         prefixSum += num;
         int need = prefixSum - k;
-
         if(counterMap.containsKey(need)) {
-            count += counterMap[need]!;
+            count += counterMap[need] ?? 0;
         }
-       counterMap[prefixSum] = (counterMap[prefixSum] ?? 0) + 1; 
-     }
-    return count; 
+        counterMap[prefixSum] = (counterMap[prefixSum] ?? 0) + 1;
+      }
+    return count;  
   }
 }
