a3fd815bdb31f72dc1542add73914024f9a76a60
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-07-07 18:56:58 +0530

    Time: 33 ms (22.73%), Space: 147.5 MB (77.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.dart
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.dart
@@ -1,19 +1,24 @@
 class Solution {
   int characterReplacement(String s, int k) {
-     Map<String, int> countFreq = {};
-     int left = 0;
-     int maxCount = 0;
+      Map<String, int> counterMap = {};
+      int maxFreq = 0;
+      int maxLength = 0;
+      int left = 0;
 
-     for(int right = 0; right < s.length ; right++) {
-        countFreq[s[right]] = (countFreq[s[right]] ?? 0) + 1;
+      for(int right = 0 ; right < s.length ; right++) {
+        
+         counterMap[s[right]] = (counterMap[s[right]] ?? 0) + 1;
 
-        if((right - left + 1) - countFreq.values.reduce(max) > k) {
-            countFreq[s[left]] = (countFreq[s[left]] ?? 0) - 1;
-            left++;
-        }
+         maxFreq = max(maxFreq, counterMap[s[right]] ?? 0);
 
-        maxCount = max(maxCount, right - left + 1);
-     }
-    return maxCount; 
+         if((right - left + 1) - maxFreq > k) {
+             counterMap[s[left]] = (counterMap[s[left]] ?? 0) - 1;
+             left++;
+         }
+
+         maxLength = max((right - left + 1), maxLength);
+      }
+
+    return maxLength;  
   }
 }
