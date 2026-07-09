f4fe97e2b2eee908f2f46cec6fed334952a5afd5
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-07-09 11:38:55 +0530

    Time: 6 ms (33.95%), Space: 148.6 MB (66.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.dart
+++ 0020-valid-parentheses/0020-valid-parentheses.dart
@@ -1,19 +1,21 @@
 class Solution {
   bool isValid(String s) {
+   List<String> stack = [];
 
-    List<String> stack = [];
-    Map<String, String> closeOpenMap = {")" : "(", "}" : "{", "]" : "["};
-
-    for(String c in s.split("")) {
-        if("([{".contains(c)) {
-            stack.add(c);
-        }
-        else {
-            if(stack.isEmpty) return false;
-            if(stack.last != closeOpenMap[c]) return false;
-            stack.removeLast();
-        }
-    } 
-   return stack.isEmpty;
+    for(String bracket in s.split("")) {
+      if(bracket == '(' || bracket == '[' || bracket == '{') {
+        stack.add(bracket);
+    }
+    else {
+      if(stack.isEmpty) return false;
+      if(bracket == ')' && stack.last != '(') return false;
+      if(bracket == '}' && stack.last != '{') return false;
+      if(bracket == ']' && stack.last != '[') return false;
+      
+      stack.removeLast();
+    }
   }
+  return stack.isEmpty;
 }
+}
+    
