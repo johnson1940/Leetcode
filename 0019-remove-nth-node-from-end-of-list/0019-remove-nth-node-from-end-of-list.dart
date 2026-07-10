157ea0131797533597b24a2015bb144cb0704fe9
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-07-10 13:30:01 +0530

    Time: 0 ms (100%), Space: 147.8 MB (78.95%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.dart
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.dart
@@ -9,21 +9,34 @@
 class Solution {
   ListNode? removeNthFromEnd(ListNode? head, int n) {
      ListNode dummy = ListNode(0, head);
-     ListNode? left = dummy;
-     ListNode? right = dummy;
+     ListNode? slow = dummy;
+     ListNode? fast = dummy;
 
-     while(n > 0 && right != null) {
-        right = right!.next;
-        n -= 1;
+     for(int i = 0; i < n ; i++) {
+        fast = fast!.next;
      }
 
-     while(right!.next != null) {
-        left = left!.next;
-        right = right.next;
+     while(fast!.next != null) {
+        slow = slow!.next;
+        fast = fast.next;
      }
 
-    left!.next = left.next!.next;
+    slow!.next = slow.next!.next;
 
-    return dummy.next;
+    return dummy.next; 
+
+    //  while(n > 0 && right != null) {
+    //     right = right!.next;
+    //     n -= 1;
+    //  }
+
+    //  while(right!.next != null) {
+    //     left = left!.next;
+    //     right = right.next;
+    //  }
+
+    // left!.next = left.next!.next;
+
+    // return dummy.next;
   }
 }
