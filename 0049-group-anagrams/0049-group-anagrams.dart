4ab5f6bd39f3cc5d8f4d62700e2c983cae47aaa6
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-02 12:24:23 +0530

    Time: 55 ms (42.04%), Space: 159.9 MB (43.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.dart
+++ 0049-group-anagrams/0049-group-anagrams.dart
@@ -1,19 +1,37 @@
 class Solution {
   List<List<String>> groupAnagrams(List<String> strs) {
-      Map<String, List<String>> map = {};
+    //   Map<String, List<String>> map = {};
 
-      for(String s in strs) {
+    //   for(String s in strs) {
          
-         List<int> count = List.filled(26, 0);
+    //      List<int> count = List.filled(26, 0);
 
-         for(int i = 0; i < s.length ; i++) {
-             count[s.codeUnitAt(i) - 97] += 1;
-         }
+    //      for(int i = 0; i < s.length ; i++) {
+    //          count[s.codeUnitAt(i) - 97] += 1;
+    //      }
 
-         String key = count.join(',');
+    //      String key = count.join(',');
 
-         map.putIfAbsent(key, () => []).add(s);
-      }
-    return map.values.toList();  
+    //      map.putIfAbsent(key, () => []).add(s);
+    //   }
+    // return map.values.toList();  
+
+    Map<String, List<String>> anagramMap = {};
+
+    for(String s in strs) {
+
+        List<int> count = List.filled(26, 0);
+
+        for(int i = 0; i < s.length ; i++) {
+            count[s.codeUnitAt(i) - 97] += 1;
+        }
+
+        String key = count.join(',');
+
+        anagramMap.putIfAbsent(key, () => []).add(s);
+    }
+   
+   return anagramMap.values.toList();
+
   }
 }
