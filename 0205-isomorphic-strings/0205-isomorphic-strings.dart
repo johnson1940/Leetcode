ba2a7c513373acd5175b36bdecf1c14ddfe50b0d
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-03 10:58:31 +0530

    Time: 14 ms (50%), Space: 151.3 MB (43.33%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.dart
+++ 0205-isomorphic-strings/0205-isomorphic-strings.dart
@@ -1,9 +1,29 @@
 class Solution {
   bool isIsomorphic(String s, String t) {
+    // Map<String, String> mapS = {};
+    // Map<String, String> mapT = {};
+
+    // for(int i = 0 ; i < s.length ; i++) {
+    //     String si = s[i];
+    //     String ti = t[i];
+
+    //     if(mapS.containsKey(si)) {
+    //         if(mapS[si] != ti) return false;
+    //     } else {
+    //         if(mapT.containsKey(ti)) {
+    //             if(mapT[ti] != si) return false;
+    //         }
+    //     }
+
+    //     mapS[si] = ti;
+    //     mapT[ti] = si;
+    // }
+    // return true;
+
     Map<String, String> mapS = {};
     Map<String, String> mapT = {};
 
-    for(int i = 0 ; i < s.length ; i++) {
+    for(int i = 0; i < s.length ; i++) {
         String si = s[i];
         String ti = t[i];
 
@@ -18,6 +38,7 @@ class Solution {
         mapS[si] = ti;
         mapT[ti] = si;
     }
-    return true;
+
+   return true; 
   }
 }
