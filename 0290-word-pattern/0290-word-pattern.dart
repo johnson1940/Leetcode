10bb7550d98f008c20962f7e1907cf7934579d4a
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-03 12:18:03 +0530

    Time: 1 ms (88.89%), Space: 150.3 MB (33.33%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.dart
+++ 0290-word-pattern/0290-word-pattern.dart
@@ -1,24 +1,19 @@
 class Solution {
   bool wordPattern(String pattern, String s) {
-      List<String> words = s.split(" ");
-      if (pattern.length != words.length) return false;
-      Map<String, String> patternMap = {};
-      Map<String, String> sMap = {};
+       List<String> words = s.split(" ");
 
-      for(int i = 0; i < pattern.length ; i++) {
-          String patternString = pattern[i];
-          String word = words[i];
+       if(words.length != pattern.length) return false;
 
-          if(patternMap.containsKey(patternString)) {
-            if(patternMap[patternString] != word) return false;
-          } else {
-            if(sMap.containsKey(word)) {
-                if(sMap[word] != patternString) return false;
-            }
-          }
-          patternMap[patternString] = word;
-          sMap[word] = patternString;
-      }
-      return true;
+       Map<String, int> charIndex = {};
+       Map<String, int> wordIndex = {};
+
+       for(int i = 0 ; i < pattern.length ; i++) {
+           
+           if(charIndex[pattern[i]] != wordIndex[words[i]]) return false;
+
+           charIndex[pattern[i]] = i;
+           wordIndex[words[i]] = i;
+       }
+    return true;   
   }
 }
