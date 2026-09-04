b26a8a6ed0cfce28f8d5fedb5fd0ead030ebcfbb
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-04 11:52:12 +0530

    Time: 2 ms (39.51%), Space: 155.5 MB (54.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.dart
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -1,13 +1,14 @@
 class Solution {
   void moveZeroes(List<int> nums) {
        int slow = 0;
-       for(int fast = 0; fast < nums.length ; fast++) {
-          if(nums[fast] != 0) {
-            int temp = nums[fast];
-            nums[fast] = nums[slow];
-            nums[slow] = temp;
-            slow++;
-          }
+
+       for(int fast = 0 ; fast < nums.length ; fast++) {
+           if(nums[fast] != 0) {
+              int temp = nums[fast];
+              nums[fast] = nums[slow];
+              nums[slow] = temp;
+              slow++;
+           }
        }
   }
 }
