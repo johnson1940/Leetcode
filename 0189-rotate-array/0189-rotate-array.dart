b73d1d027fa8dd297694c98ea89f27798715bfe6
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-07 17:59:20 +0530

    Time: 4 ms (48.57%), Space: 266.1 MB (20%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.dart
+++ 0189-rotate-array/0189-rotate-array.dart
@@ -1,32 +1,21 @@
 class Solution {
   void rotate(List<int> nums, int k) {
-    
-    // nums = [1,2,3]  n=3
-
-    // rotate 3 times → [1,2,3]  ← same as start!
-    // rotate 4 times → [3,1,2]  ← same as rotating 1 time
-    // rotate 6 times → [1,2,3]  ← same as rotating 0 times
-    k = k % nums.length;
-    int start = 0;
-    int end = nums.length - 1;
-
-    // reversing the entire array
-    reverse(nums, start, end);
-
-    // reversing the first k elements
-    reverse(nums, start, k-1);
-
-    // reversing the n-k elements
-    reverse(nums, k , end);
- }
-
-  void reverse(List<int> nums, int start, int end) {
-    while(start < end) {
-    int temp = nums[start];
-    nums[start] = nums[end];
-    nums[end] = temp;
-    start++;
-    end--;
+       k = k % nums.length;
+       int left = 0;
+       int right = nums.length - 1;
+       reverse(nums, left, right);
+       reverse(nums, left , k - 1);
+       reverse(nums, k , right);
    }
-  }
+
+
+   void reverse(List<int> nums, int left, int right) {
+       while(left < right) {
+           int temp = nums[left];
+           nums[left] = nums[right];
+           nums[right] = temp;
+           left++;
+           right--;
+       }
+   }
 }
