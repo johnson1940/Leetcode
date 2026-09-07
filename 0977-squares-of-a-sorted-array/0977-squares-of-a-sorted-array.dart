e43eb64fa6d03bf515d2e92d2bc55ce2925d1b87
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-07 10:05:41 +0530

    Time: 1 ms (100%), Space: 159.2 MB (7.32%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.dart
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.dart
@@ -1,23 +1,25 @@
 class Solution {
   List<int> sortedSquares(List<int> nums) { 
-    List<int> squareList = List.filled(nums.length, 0);
-    int left = 0;
-    int right = nums.length - 1;
+      
+      List<int> squareList = List.filled(nums.length, 0);
 
-    for(int i = nums.length - 1; i >= 0; i--) {
-        int leftSquare = nums[left] * nums[left];
-        int rightSquare = nums[right] * nums[right];
+      int left = 0;
+      int right = nums.length - 1;
 
-        if(leftSquare > rightSquare) {
+      for(int i = squareList.length - 1 ; i >= 0 ; i--) {
+         int leftSquare = nums[left] * nums[left];
+         int rightSquare = nums[right] * nums[right];
+
+         if(leftSquare > rightSquare) {
             squareList[i] = leftSquare;
             left++;
-        }
-        else {
+         }
+         else {
             squareList[i] = rightSquare;
             right--;
-        }
-    }
-    return squareList;
+         }
+      }
+    return squareList;  
   }
       
 }
