7b813caf1f6d76a18738083be4f18b6767398a2b
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-08 12:59:57 +0530

    Time: 43 ms (77.98%), Space: 158.8 MB (57.8%) - LeetHub

--- 0015-3sum/0015-3sum.dart
+++ 0015-3sum/0015-3sum.dart
@@ -1,21 +1,26 @@
 class Solution {
   List<List<int>> threeSum(List<int> nums) {
-    List<List<int>> result = [];
-    nums.sort();
-    Set<String> seen = {};
+     nums.sort();
 
-    for(int i = 0 ; i < nums.length - 2 ; i++) {
+     List<List<int>> result = [];
+
+     for(int i = 0 ; i < nums.length - 2; i++) {
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+
         int left = i + 1;
+        
         int right = nums.length - 1;
 
         while(left < right) {
             int sum = nums[i] + nums[left] + nums[right];
-            String key = '${nums[i]},${nums[left]},${nums[right]}';
+
             if(sum == 0) {
-                if(!seen.contains(key)) {
-                  seen.add(key);
-                  result.add([nums[i], nums[left], nums[right]]);
-                }
+                result.add([nums[i], nums[left], nums[right]]);
+
+                while(left < right && nums[left] == nums[left + 1]) left++;
+
+                while(left < right && nums[right] == nums[right - 1]) right--;
+
                 left++;
                 right--;
             }
@@ -26,7 +31,7 @@ class Solution {
                 left++;
             }
         }
-    }
-   return result;
+     }
+    return result; 
   }
 }
