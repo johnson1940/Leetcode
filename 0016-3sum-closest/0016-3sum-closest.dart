3039167dd9f5681127b0ee8112901b819d5cd5d1
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-08 13:27:08 +0530

    Time: 18 ms (100%), Space: 155.5 MB (17.5%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.dart
+++ 0016-3sum-closest/0016-3sum-closest.dart
@@ -1,26 +1,29 @@
 class Solution {
   int threeSumClosest(List<int> nums, int target) {
-    nums.sort();
-    int closetSum = nums[0] + nums[1] + nums[2];
+      nums.sort();
 
-  for (int i = 0; i < nums.length - 2; i++) {
-    int left = i + 1;
-    int right = nums.length - 1;
+      int closetSum = nums[0] + nums[1] + nums[2];
 
-    while (left < right) {
-      int sum = nums[i] + nums[left] + nums[right];
-      if(nums.length == 3) return sum;
+      for(int i = 0; i < nums.length - 2;  i++) {
+          
+          int left = i + 1;
+          int right = nums.length - 1;
 
-      if ((sum - target).abs() < (closetSum - target).abs()) {
-        closetSum = sum;
-      } else if (sum > target) {
-        right--;
-      } else {
-        left++;
+          while(left < right) {
+
+             int sum = nums[i] + nums[left] + nums[right];
+             
+             if((sum - target).abs() < (closetSum - target).abs()) {
+                closetSum = sum;
+             }
+
+             if(sum == target) return sum;
+
+             if(sum > target) right--;
+             else left++;
+
+          }
       }
-    }
-  }
-
-  return closetSum;
-  }
+    return closetSum;  
+  } 
 }
