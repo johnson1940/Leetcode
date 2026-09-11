da04024143de5364fb27adbdc1c394d657cfe6b8
Johnson <116719711+johnson1940@users.noreply.github.com>
2026-09-11 10:49:47 +0530

    Time: 4 ms (100%), Space: 148.5 MB (100%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.dart
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.dart
@@ -1,39 +1,40 @@
 class Solution {
   List<int> findAnagrams(String s, String p) {
-    if(p.length > s.length) return []; 
-    List<int> result = [];
-    Map<String, int> sMap = {};
-    Map<String, int> pMap = {};
+      List<int> result = [];
 
-    /// Building the frequency of first map
-    for(int i = 0; i < p.length ; i++) {
-        pMap[p[i]]= (pMap[p[i]] ?? 0) + 1;
-    }
+      if(p.length > s.length) return result;
 
-    /// Building the frequency for the first window
-    for(int i = 0; i < p.length ; i++) {
-        sMap[s[i]] = (sMap[s[i]] ?? 0) + 1;
-    }
+      List<int> sCount = List.filled(26, 0);;
+      List<int> pCount = List.filled(26, 0);;
 
-    /// Checking the map Equality
-    if(mapsEqual(sMap, pMap)) result.add(0);
+      int aCode = 'a'.codeUnitAt(0);
 
-    
-    for(int R = p.length ;  R < s.length ; R++) {
-        sMap[s[R]] = (sMap[s[R]] ?? 0) + 1;
-        String left = s[R - p.length];
-        sMap[left] = (sMap[left] ?? 0) - 1;
-        if(sMap[left] == 0) sMap.remove(left);
-        if(mapsEqual(sMap, pMap)) result.add(R - p.length + 1);
+      for(int i = 0; i < p.length ; i++) {
+          pCount[p.codeUnitAt(i) - aCode]++;
+          sCount[s.codeUnitAt(i) - aCode]++;
+      }
+      
+      if(_listsEqual(pCount, sCount)) {
+         result.add(0);
+      }
+
+      for(int i = p.length; i < s.length; i++) {
+
+        sCount[s.codeUnitAt(i) - aCode]++;
+
+        sCount[s.codeUnitAt(i - p.length) - aCode]--;
+
+        if(_listsEqual(pCount, sCount)) {
+            result.add(i - p.length + 1);
+        }
     }
    return result;
   }
 
-  bool mapsEqual(Map<String, int> a, Map<String, int> b) {
-    if (a.length != b.length) return false;
-      for (String key in a.keys) {
-      if (a[key] != b[key]) return false;
-    }
-     return true;
+  bool _listsEqual(List<int> a, List<int> b) {
+       for (int i = 0; i < 26; i++) {
+          if (a[i] != b[i]) return false;
+       }
+    return true;
   }
 }
